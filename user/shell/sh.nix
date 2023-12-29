ae43bd73cbc17afd56b2efd2c449a725ec09d57a
Cryxtalix <fgx8p9t@protonmail.com>
2023-12-29 21:14:29 +0800

    Allow inheriting nix config path

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -1,19 +1,26 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, configDir, ... }:
+  
 {
   programs.bash = {
     enable = true;
     shellAliases = {
-      print_hello = "echo hello";
-      nix-flake-up = "nix flake update ~/.flakes";
-      nix-system-up = "sudo nixos-rebuild switch --flake ~/.flakes";
-      nix-home-up = "home-manager switch --flake ~/.flakes";
-      nix-full-update = "nix-flake-up && nix-system-up && nix-home-up";
+      print_hello = "echo " + configDir;
+      nix-flake-up = "nix flake update " + configDir;
+      nix-system-up = "sudo nixos-rebuild switch --flake " + configDir;
+      nix-home-up = "home-manager switch --flake " + configDir;
+      nix-full-up = "nix-flake-up && nix-system-up && nix-home-up";
     };
     initExtra = ''
       nix-dev() {
-        nix develop ~/.flakes/.#"$1"
+        nix develop ${configDir}/.#"$1"
       }
     '';
+
+    /* initExtra = ''
+      nix-dev() {
+        nix develop ~/.flakes/.#"$1"
+      }
+    ''; */
+
   };
 }
