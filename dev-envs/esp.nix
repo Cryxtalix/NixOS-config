4581094dadc8327bc750d36fc9df9112fe3bf1b0
Cryxtalix <fgx8p9t@protonmail.com>
2024-01-12 01:07:52 +0800

    Added esp dev env

--- dev-envs/esp.nix
+++ dev-envs/esp.nix
@@ -13,6 +13,11 @@ pkgs.mkShell {
   ]);
   shellHook = ''
     . $HOME/Workspace/ESP32/esp-idf/export.sh
+    alias idfa="idf.py build && idf.py flash && idf.py monitor"
+    alias idfbf="idf.py build && idf.py flash"
+    alias idfb="idf.py build"
+    alias idff="idf.py flash"
+    alias idfm="idf.py monitor"
     echo "Started ESP32 development environment..."
   '';
 }
