c1442e74c7cfb2a881cb3a1fa3b628506546482b
Cryxtalix <fgx8p9t@protonmail.com>
2024-01-28 23:33:45 +0800

    Fix

--- install_nix_pm.sh
+++ install_nix_pm.sh
@@ -4,10 +4,12 @@
 # Installs minimal home-manager config
 
 sh <(curl -L https://nixos.org/nix/install) --no-daemon &&
+. /home/cryxtalix/.nix-profile/etc/profile.d/nix.sh &&
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager &&
 nix-channel --update &&
 nix-shell '<home-manager>' -A install &&
-mkdir ~/.config/nix &&
+sudo mkdir ~/.config/nix &&
+sudo chown cryxtalix ~/.config/nix &&
 echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf &&
 cd ~/NixOS-config &&
 home-manager switch -b backup --flake .#minimal
