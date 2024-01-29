4b82f9dc5605225e891d095a491a7e90506dad7b
Cryxtalix <fgx8p9t@protonmail.com>
2024-01-29 23:16:23 +0800

    Fix

--- install_nix_pm.sh
+++ install_nix_pm.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 # ONLY FOR FIRST TIME INSTALLING NIX PACKAGE MANAGER!
 # Installs Nix package manager and home-manager
-# Installs minimal home-manager config
+# Installs minimal home-manager config  q
 
 if [[ $EUID -ne 0 ]]; then
     echo "This script requires sudo privileges. Please run with sudo."
     exit 1
 fi
 
-sh <(curl -L https://nixos.org/nix/install) --no-daemon &&
-. ~/.nix-profile/etc/profile.d/nix.sh &&
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager &&
-nix-channel --update &&
-nix-shell '<home-manager>' -A install &&
+sudo -u $(logname) sh <(curl -L https://nixos.org/nix/install) --no-daemon &&
+sudo -u $(logname) . ~/.nix-profile/etc/profile.d/nix.sh &&
+sudo -u $(logname) nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager &&
+sudo -u $(logname) nix-channel --update &&
+sudo -u $(logname) nix-shell '<home-manager>' -A install &&
 sudo mkdir ~/.config/nix &&
 sudo chown $(whoami) ~/.config/nix &&
-echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf &&
-home-manager switch -b backup --flake ~/NixOS-config/#minimal
+sudo -u $(logname) echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf &&
+sudo -u $(logname) home-manager switch -b backup --flake ~/NixOS-config/#minimal
