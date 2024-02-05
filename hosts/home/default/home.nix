ebad092dcea946fa422035245c8e2c0c4e97b567
Cryxtalix <fgx8p9t@protonmail.com>
2024-02-05 22:05:55 +0800

    Refactored home.nix to move imports to the top

--- hosts/home/default/home.nix
+++ hosts/home/default/home.nix
@@ -1,93 +1,72 @@
 { config, pkgs, pkgs_unstable, username, is_nixos, configDir, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
-  home.username = username;
-  home.homeDirectory = "/home/${username}";
-  home.stateVersion = "23.11"; # Do not change
-  programs.home-manager.enable = true; # Do not change
-  targets.genericLinux.enable = if is_nixos then false else true;
-
-  # Programs added here are managed by home-manager
   imports = [
     ../../../packages
   ];
 
-  # Installed packages
-  home.packages = with pkgs; [
-    arduino
-    chromium
-    ddrescue
-    discord
-    distrobox
-    element-desktop
-    ffmpeg
-    firefox
-    pkgs_unstable.foliate
-    gittyup
-    gnome.gnome-boxes
-    gnome.gnome-tweaks
-    gnome-extension-manager
-    gnomeExtensions.alphabetical-app-grid
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.caffeine
-    gnomeExtensions.gsconnect # Check https://userbase.kde.org/KDEConnect#Troubleshooting
-    htop
-    libreoffice
-    libvirt
-    lutris
-    marker
-    neofetch
-    nerdfonts
-    nurl
-    openssl
-    p7zip
-    podman
-    python3
-    qbittorrent-qt5
-    qemu
-    realvnc-vnc-viewer
-    sops
-    switcheroo-control
-    telegram-desktop
-    unrar
-    virtualbox
-    virt-manager
-    vlc
-    vscode
-    wget
-    wineWowPackages.stable
-    winetricks
-    xivlauncher
-  ];
+  nixpkgs.config.allowUnfree = true;
+  programs.home-manager.enable = true; # Do not change
+  targets.genericLinux.enable = if is_nixos then false else true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  home = {
+    username = username;
+    homeDirectory = "/home/${username}";
+    stateVersion = "23.11"; # Do not change
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+    packages = with pkgs; [
+      arduino
+      chromium
+      ddrescue
+      discord
+      distrobox
+      element-desktop
+      ffmpeg
+      firefox
+      pkgs_unstable.foliate
+      gittyup
+      gnome.gnome-boxes
+      gnome.gnome-tweaks
+      gnome-extension-manager
+      gnomeExtensions.alphabetical-app-grid
+      gnomeExtensions.blur-my-shell
+      gnomeExtensions.caffeine
+      gnomeExtensions.gsconnect # Check https://userbase.kde.org/KDEConnect#Troubleshooting
+      htop
+      libreoffice
+      libvirt
+      lutris
+      marker
+      neofetch
+      nerdfonts
+      nurl
+      openssl
+      p7zip
+      podman
+      python3
+      qbittorrent-qt5
+      qemu
+      realvnc-vnc-viewer
+      sops
+      switcheroo-control
+      telegram-desktop
+      unrar
+      virtualbox
+      virt-manager
+      vlc
+      vscode
+      wget
+      wineWowPackages.stable
+      winetricks
+      xivlauncher
+    ];
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/cryxtalix/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+    file = {
+      # dotfiles
+    };
+
+    sessionVariables = {
+      # EDITOR = "emacs";
+    };
   };
 }
