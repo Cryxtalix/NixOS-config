3f86a5542338e13b0ae1bb6a5b022903fd86369a
Cryxtalix <fgx8p9t@protonmail.com>
2024-02-11 02:10:40 +0800

    Added more modules

--- hosts/system/AcerSwift3/configuration.nix
+++ hosts/system/AcerSwift3/configuration.nix
@@ -3,9 +3,8 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ../modules
     ../modules/nvidia_drivers
-    ../modules/firewall
-    ../modules/sound
   ];
 
   # ---------------------------BOOTLOADER START---------------------------
@@ -30,16 +29,6 @@
     };
     package = pkgs.nixFlakes;
   };
-
-  networking.hostName = hostname; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = timezone;
--- hosts/system/NixVM/configuration.nix
+++ hosts/system/NixVM/configuration.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ../modules/sound
+    ../modules
   ];
 
   # ---------------------------BOOTLOADER START---------------------------
@@ -28,16 +28,6 @@
     };
     package = pkgs.nixFlakes;
   };
-
-  networking.hostName = hostname; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = timezone;
