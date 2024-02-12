8c73d654e0a42b9e881937fdec8950dddc2c81f4
Cryxtalix <fgx8p9t@protonmail.com>
2024-02-12 21:29:47 +0800

    Temp neovim conf

--- packages/neovim/default.nix
+++ packages/neovim/default.nix
@@ -3,9 +3,23 @@
 {
   programs.neovim = {
     enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    extraLuaConfig = ''
+    '';
+
     plugins = with pkgs.vimPlugins; [
       nvchad
       nvchad-ui
+
+      /* {
+        plugin = something;
+        type = "lua";
+        config = "";
+      } */
+
     ];
   };
 }
