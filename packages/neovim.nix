0a5542b62466954bbb5a45a719aa2d36202f0442
Cryxtalix <fgx8p9t@protonmail.com>
2024-02-14 04:52:44 +0800

    Initial neovim config

--- packages/neovim.nix
+++ packages/neovim.nix
@@ -8,19 +8,78 @@
     vimAlias = true;
     vimdiffAlias = true;
 
+    plugins = with pkgs_unstable.vimPlugins; [
+      {
+        # Theme
+        plugin = catppuccin-nvim;
+        type = "lua";
+        config = ''
+          require("catppuccin").setup()
+          vim.cmd.colorscheme "catppuccin"
+        '';
+      }
+
+      {
+        # File searching
+        plugin = telescope-nvim;
+        type = "lua";
+        config = ''
+          local builtin = require("telescope.builtin")
+          vim.keymap.set('n', '<C-f>', builtin.find_files, {})
+          vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+        '';
+      }
+
+      {
+        # Syntax highlighting
+        plugin = nvim-treesitter.withAllGrammars;
+        type = "lua";
+        config = ''
+          require('nvim-treesitter.configs').setup({
+            highlight = { enable = true },
+            indent = { enable = true },
+          })
+        '';
+      }
+
+      {
+        # File tree
+        plugin = neo-tree-nvim;
+        type = "lua";
+        config = ''
+          vim.keymap.set('n', '<C-t>', ':Neotree filesystem reveal left<CR>', {})
+          require('neo-tree').setup {
+            filesystem = {
+              filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_by_name = {
+                  ".DS_Store",
+                  "thumbs.db",
+                },
+                never_show = {
+                  ".git"
+                },
+              },
+            }
+          }
+        '';
+      }
+
+      # Dependencies
+      plenary-nvim
+      nvim-web-devicons
+      nui-nvim
+    ];
+
     extraLuaConfig = ''
+      vim.g.mapleader = " "
     '';
 
-    plugins = with pkgs_unstable.vimPlugins; [
-      nvchad
-      nvchad-ui
-
-      /* {
-        plugin = something;
-        type = "lua";
-        config = "";
-      } */
-
+    extraPackages = with pkgs; [
+      ripgrep # Requirement for telescope
     ];
   };
 }
