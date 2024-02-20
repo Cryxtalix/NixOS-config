ef8544d4a6774d76b5cbd91313abd55332191694
Cryxtalix <fgx8p9t@protonmail.com>
2024-02-21 03:43:30 +0800

    Fix script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./modules/disko.nix &&
-nixos-generate-config --no-filesystems --root /mnt
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ~/NixOS-config/modules/disko.nix &&
+sudo nixos-generate-config --no-filesystems --root /mnt &&
+sudo nixos-install --root /mnt --flake
