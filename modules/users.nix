b7d1b34e548b56ead11f2a1858c91d6d5f61bbc3
Cryxtalix <fgx8p9t@protonmail.com>
2024-03-01 15:48:09 +0800

    Final test with user password with sops

--- modules/users.nix
+++ modules/users.nix
@@ -11,8 +11,7 @@
       cryxtalix = {
         description = "Main user";
         isNormalUser = true;
-        hashedPassword = "$y$j9T$YgyOvaKbDgbYy4QLR5k/L1$JFoAyqp0etqD/FsMjubdjTBQ5PV/nr/e89GFVCZjAm/";
-        #hashedPasswordFile = config.sops.secrets.cryxtalix_password.path;
+        hashedPasswordFile = config.sops.secrets.cryxtalix_password.path;
         extraGroups = [
           "networkmanager"
           "wheel"
@@ -23,5 +22,5 @@
     };
   };
 
-  #sops.secrets.cryxtalix_password.neededForUsers = true;
+  sops.secrets.cryxtalix_password.neededForUsers = true;
 }
