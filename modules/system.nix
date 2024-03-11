a6436a4c2f8b591ec064a2bd04861e5c467294d2
Cryxtalix <fgx8p9t@protonmail.com>
2024-03-11 22:04:23 +0800

    Fix

--- modules/system.nix
+++ modules/system.nix
@@ -43,7 +43,7 @@
 
   sops = {
     age.keyFile = "${user.homeDir}/.config/sops/age/keys.txt";
-    defaultSopsFile = ../user/${user_profile}/secrets.yml;
+    defaultSopsFile = ../user/${user_profile}/secrets.yaml;
     defaultSopsFormat = "yaml";
   };
 
