78ea427b037d0dc63538004fa507b71459b874f1
Cryxtalix <fgx8p9t@protonmail.com>
2024-03-21 02:09:50 +0800

    Fix gpg and sops

--- packages/gnupg.nix
+++ packages/gnupg.nix
@@ -1,24 +1,6 @@
 { is_nixos, ... }:
 
 {
-  /* services.pcscd.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-    pinentryFlavor = "gnome3"; # https://search.nixos.org/options?channel=23.11&show=programs.gnupg.agent.pinentryFlavor&from=0&size=50&sort=relevance&type=packages&query=gnupg.agent
-  }; */
-
-  # OR
-
-  /* services.gnome-keyring.enable = true;
-
-  programs.gpg.enable = true;
-  services.gpg-agent = {
-    enable = true;
-    pinentryFlavor = "gnome3";
-  }; */
-
-
   home.file = {
     ".gnupg/gpg.conf".text = ''
       use-agent 
@@ -36,4 +18,21 @@
       max-cache-ttl 86400
     '';
   };
+
+  /* services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "gnome3"; # https://search.nixos.org/options?channel=23.11&show=programs.gnupg.agent.pinentryFlavor&from=0&size=50&sort=relevance&type=packages&query=gnupg.agent
+  }; */
+
+  # OR
+
+  /* services.gnome-keyring.enable = true;
+
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "gnome3";
+  }; */
 }
