ca04f206b5c09cad078d339411c85f55495cb678
Cryxtalix <fgx8p9t@protonmail.com>
2024-03-28 23:20:32 +0800

    Test overlay

---
+++ overlays/foliate.nix
@@ -0,0 +1,14 @@
+final: prev:
+{
+  foliate = prev.foliate.overrideAttrs (
+    old: {
+      src = prev.fetchFromGitHub {
+        owner = "johnfactotum";
+        repo = "foliate";
+        rev = "refs/tags/3.1.0";
+        hash = "sha256-6cymAqQxHHoTgzEyUKXC7zV/lUEJfIG+54+tLsc9iHo=";
+        fetchSubmodules = true;
+      };
+    }
+  )
+}
