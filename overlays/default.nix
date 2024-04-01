01abdbbcbd097119f92177c698a5fdbb3c2f07cb
Cryxtalix <fgx8p9t@protonmail.com>
2024-04-01 15:52:00 +0800

    Vscode overlay to previous version

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,12 +1,14 @@
 final: prev: 
 {
-  /* foliate = prev.foliate.overrideAttrs (old: {
-    src = prev.fetchFromGitHub {
-      owner = "johnfactotum";
-      repo = "foliate";
-      rev = "refs/tags/3.1.0";
-      hash = "sha256-6cymAqQxHHoTgzEyUKXC7zV/lUEJfIG+54+tLsc9iHo=";
-      fetchSubmodules = true;
+  vscode = prev.vscode.overrideAttrs (old: 
+  rec {
+    version = "1.85.2";
+    rev = "8b3775030ed1a69b13e4f4c628c612102e30a681";
+
+    src = prev.fetchurl {
+      name = "VSCode_1.85.2_linux-x64.tar.gz";
+      url = "https://update.code.visualstudio.com/1.85.2/linux-x64/stable";
+      sha256 = "0v702nvv971rwv1grp921ys2d1ig0aq0di7idc1lfikl5ka9b4wa";
     };
-  }); */
+  });
 }
